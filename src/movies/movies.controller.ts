ba906df997cb9e546c68e7ce47c3a67fb1b88cb2
나상우 <robbyra@gmail.com>
2021-01-30 14:39:20 +0900

    Add some more routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,9 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
   @Get('/movies')
   getAll(): string {
     return 'This will return all movieS';
+  }
+
+  @Get('/:id')
+  getMovieById(@Param('id', ParseIntPipe) id: string): string {
+    return `This is info about a movie. (ID : ${id}), ${typeof id}`;
+  }
+
+  @Post()
+  createMovie(): string {
+    return 'This will create a movie.';
+  }
+
+  @Delete('/:id')
+  deleteMovie(@Param('id', ParseIntPipe) id: number): string {
+    return `This will remove a movie.(ID : ${id}`;
+  }
+
+  @Put('/:id')
+  updateMovie(@Param('id', ParseIntPipe) id: number): string {
+    return `This will update a movie.(ID : ${id})`;
+  }
+
+  @Patch('/:id')
+  updateMovieInfo(@Param('id', ParseIntPipe) id: number): string {
+    return `This will update some info about movie.(ID : ${id})`;
   }
 }
