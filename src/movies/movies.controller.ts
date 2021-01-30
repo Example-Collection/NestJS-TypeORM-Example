9b41c1aeca0921a285b63ac5b5f0c85f6703cf35
나상우 <robbyra@gmail.com>
2021-01-30 14:08:13 +0900

    Sample API path

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  @Get('/movies')
+  getAll(): string {
+    return 'This will return all movieS';
+  }
+}
