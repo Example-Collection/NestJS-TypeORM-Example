2300ec658d61859d89032ca8fc6ab6419568e98f
sangwoo-98 <robbyra@gmail.com>
2021-02-15 16:34:39 +0900

    Create repository method for log-in

--- src/entities/user.repository.ts
+++ src/entities/user.repository.ts
@@ -2,4 +2,11 @@ import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  findByEmailAndPassword(email: string, password: string): Promise<User> {
+    return this.createQueryBuilder()
+      .where('u.email = :email', { email })
+      .andWhere('u.password = :password', { password })
+      .getOne();
+  }
+}
