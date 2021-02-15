2660e1b28b8c1fd85fc483ad90fcd6433f80e068
sangwoo-98 <robbyra@gmail.com>
2021-02-15 16:30:34 +0900

    Simple JWT token util functions

--- src/utils/auth/jwt-token-util.ts
+++ src/utils/auth/jwt-token-util.ts
@@ -1,5 +1,15 @@
 import * as jwt from 'jsonwebtoken';
 
 export function generateAccessToken(userId: number): string {
-  const token: string = jwt;
+  return jwt.sign(
+    {
+      userId: userId,
+      exp: Math.floor(Date.now() / 1000) + 86400000,
+    },
+    process.env.JWT_SERCET_KEY,
+  );
+}
+
+export function extractUserId(token: string) {
+  return jwt.verify(token, process.env.JWT_SERCET_KEY);
 }
