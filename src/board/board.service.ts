f474503b0511460b07e3fcb80da39b18cdea2f41
sangwoo-98 <robbyra@gmail.com>
2021-02-17 19:11:43 +0900

    BoardService#saveBoard()

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,6 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UserRepository } from '../entities/user/user.repository';
 import { BoardRepository } from '../entities/board/board.repository';
+import { BoardCreateDto } from './dtos/create-board-dto';
+import { BoardInfoResponseDto } from './dtos/board-info.dto';
+import { extractUserId } from '../utils/auth/jwt-token-util';
+import { Board } from '../entities/board/board.entity';
 
 @Injectable()
 export class BoardService {
@@ -8,4 +16,27 @@ export class BoardService {
     private readonly boardRepository: BoardRepository,
     private readonly userRepository: UserRepository,
   ) {}
+
+  // TODO : Paging 처리해서 가져오기(Token 불필요)
+
+  async saveBoard(
+    dto: BoardCreateDto,
+    token: string,
+    userId: number,
+  ): Promise<BoardInfoResponseDto> {
+    if (userId !== extractUserId(token)) {
+      throw new ForbiddenException(
+        'userId in parameter and token is different.',
+      );
+    }
+    const user = await this.userRepository.findOne(userId);
+    if (!!user) {
+      const board = new Board();
+      board.setContent = dto.content;
+      board.setTitle = dto.title;
+      board.user = user;
+      const savedBoard = await this.boardRepository.save(board);
+      return new BoardInfoResponseDto(user, savedBoard);
+    } else throw new NotFoundException('UserId is invalid.');
+  }
 }
--- src/board/dtos/board-info.dto.ts
+++ src/board/dtos/board-info.dto.ts
@@ -3,15 +3,19 @@ import { User } from 'src/entities/user/user.entity';
 
 export class BoardInfoResponseDto {
   constructor(user: User, board: Board) {
+    this.boardId = board.getBoard_id;
     this.title = board.getTitle;
     this.content = board.getContent;
     this.createdAt = board.getCreatedAt;
     this.lastModifiedAt = board.getLastModifiedAt;
+    this.userId = user.getUser_id;
     this.name = user.getName;
   }
+  boardId: number;
   title: string;
   content: string;
   createdAt: Date;
   lastModifiedAt: Date;
+  userId: number;
   name: string;
 }
