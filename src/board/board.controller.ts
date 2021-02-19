bc03800347a45e59ba1d4185717641a1a2b2e9e1
sangwoo-98 <robbyra@gmail.com>
2021-02-19 10:35:20 +0900

    Map [GET] /board/{boardId} to controller

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { BoardService } from './board.service';
+import { BoardInfoResponseDto } from './dtos/board-info.dto';
 
 @Controller('board')
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
+  @Get('/:boardId')
+  getByBoardId(
+    @Param('boardId') boardId: number,
+  ): Promise<BoardInfoResponseDto> {
+    return this.boardService.getByBoardId(boardId);
+  }
 }
